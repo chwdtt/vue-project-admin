fd3da39481cc3a31ba2243ad47a66ede5769c65e
chenhewen <chenhewen@qq.com>
2019-01-24 14:19:45 +0800

    test2

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    123123123
   </div>
 </template>
 
